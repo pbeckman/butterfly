8ed21193e251217f84d1a11b0138e7791751c063
Samuel F. Potter <sfp@cims.nyu.edu>
2024-07-05 17:33:08 -0700

    refactor cheb_cov

--- examples/covariance/cheb_cov.c
+++ examples/covariance/cheb_cov.c
@@ -15,37 +15,50 @@
 #include <stdlib.h>
 #include <string.h>
 
-static BfReal kappa = BF_NAN;
-static BfReal nu = BF_NAN;
+struct {
+  BfSize p;
+  bool lumping;
+  BfReal kappa;
+  BfReal nu;
+  BfSize numSamples;
+  BfTrimesh *trimesh;
+  BfSize n;
+  BfMat *L;
+  BfMat *M;
+  BfReal lamMax;
+  BfCheb gammaCheb;
+  union {
+    BfMat *MFactLumped;
+    BfChol *MFactCholesky;
+  };
+} context;
 
 static BfReal gamma_(BfReal lambda) {
-  if (nu == 0) {
+  if (context.nu == 0) {
     // squared exponential spectral density function
-    return exp(-kappa*lambda*lambda);
+    return exp(-context.kappa*lambda*lambda);
   } else {
     // Matern spectral density function, normalized so g(0) = 1
-    return pow(fabs(1 + kappa*kappa*lambda), -nu/4 - 1./2);
+    return pow(fabs(1 + pow(context.kappa, 2)*lambda), -context.nu/4 - 1./2);
   }
 }
 
-static BfVec *applyS(BfMat const *L, BfMat const *Mfact, BfVec *w, int lumping) {
+static BfVec *applyS(BfVec *w) {
   BfVec *y    = NULL;
   BfVec *tmp1 = NULL;
   BfVec *tmp2 = NULL;
-  if (lumping) {
-    // Mfact is inverse diagonal sqrt C^2 = M^{-1}
+  if (context.lumping) {
+    // MFact is inverse diagonal sqrt C^2 = M^{-1}
     // apply C * L * C
-    tmp1 = bfMatMulVec(Mfact, w);
-    tmp2 = bfMatMulVec(L, tmp1);
-    y    = bfMatMulVec(Mfact, tmp2);
+    tmp1 = bfMatMulVec(context.MFactLumped, w);
+    tmp2 = bfMatMulVec(context.L, tmp1);
+    y    = bfMatMulVec(context.MFactLumped, tmp2);
   } else {
-    // Mfact is cholesky factor C*C^T = M.
+    // MFact is cholesky factor C*C^T = M.
     // we want to apply C^{-1} * L * C^{-T}
-
-    BF_DIE();
-    // tmp1 = bfCholCsrRealSolveVec(bfMatToCholCsrReal(Mfact), w); // TODO: needs to be C^{-T} not C^{-1} !
-    // tmp2 = bfMatMulVec(L, tmp1);
-    // y    = bfCholCsrRealSolveVec(Mfact, tmp2);
+    tmp1 = bfCholFacSolveVec(context.MFactCholesky, w, /* transposed: */ true);
+    tmp2 = bfMatMulVec(context.L, tmp1);
+    y    = bfCholFacSolveVec(context.MFactCholesky, tmp2, /* transposed: */ false);
   }
 
   if (tmp1 != NULL) bfVecDelete(&tmp1);
@@ -54,27 +67,26 @@ static BfVec *applyS(BfMat const *L, BfMat const *Mfact, BfVec *w, int lumping) 
   return y;
 }
 
-static BfVec *chebmul(BfCheb const *cheb, BfMat const *L, BfMat const *Mfact, BfVec *w, int lumping) {
-  BfReal const *c = cheb->c;
+static BfVec *chebmul(BfVec *w) {
+  BfReal const *c = context.gammaCheb.c;
 
-  BF_ASSERT(cheb->a == 0);
-  BfReal lamMax = cheb->b;
+  BF_ASSERT(context.gammaCheb.a == 0);
+  BfReal lamMax = context.gammaCheb.b;
 
-  BfSize n = bfMatGetNumRows(L);
-  BfVec *x = bfVecRealToVec(bfVecRealNewWithValue(n, 0));
+  BfVec *x = bfVecRealToVec(bfVecRealNewWithValue(context.n, 0));
 
   BfVec *y2 = bfVecCopy(w);
 
   bfVecDaxpy(x, c[0], y2);
 
-  BfVec *y1 = applyS(L, Mfact, w, lumping);
+  BfVec *y1 = applyS(w);
 
   bfVecDscal(y1, 2/lamMax);
   bfVecDaxpy(y1, -1, w);
   bfVecDaxpy(x, c[1], y1);
 
-  for (BfSize k = 2; k < cheb->order; ++k) {
-    BfVec *y = applyS(L, Mfact, y1, lumping);
+  for (BfSize k = 2; k < context.gammaCheb.order; ++k) {
+    BfVec *y = applyS(y1);
     bfVecDscal(y, 4/lamMax);
     bfVecDaxpy(y, -2, y1);
     bfVecDaxpy(y, -1, y2);
@@ -92,16 +104,14 @@ static BfVec *chebmul(BfCheb const *cheb, BfMat const *L, BfMat const *Mfact, Bf
   return x;
 }
 
-static BfVec *sample_z(BfCheb const *cheb, BfMat const *L, BfMat const *Mfact, int lumping) {
-  BfSize n = bfMatGetNumRows(L);
-  BfVec *w = bfVecRealToVec(bfVecRealNewRandn(n));
-  BfVec *x = chebmul(cheb, L, Mfact, w, lumping);
+static BfVec *sample_z(void) {
+  BfVec *w = bfVecRealToVec(bfVecRealNewRandn(context.n));
+  BfVec *x = chebmul(w);
   BfVec *z = NULL;
-  if (lumping) {
-    z = bfMatMulVec(Mfact, x);
+  if (context.lumping) {
+    z = bfMatMulVec(context.MFactLumped, x);
   } else {
-    BF_DIE();
-    // z = bfCholCsrRealSolveVec(Mfact, x); // TODO: needs to be C^{-T} not C^{-1} !
+    z = bfCholFacSolveVec(context.MFactCholesky, x, /* transposed: */ true);
   }
 
   bfVecDelete(&w);
@@ -109,147 +119,136 @@ static BfVec *sample_z(BfCheb const *cheb, BfMat const *L, BfMat const *Mfact, i
   return z;
 }
 
-static BfVec *cov_matvec(BfVec *v, BfCheb const *cheb, BfMat const *L, BfMat const *Mfact, int lumping) {
+static BfVec *cov_matvec(BfVec *v) {
   BfVec *tmp = NULL;
-  if (lumping) {
-    tmp = bfMatMulVec(Mfact, v);
+  if (context.lumping) {
+    tmp = bfMatMulVec(context.MFactLumped, v);
   } else {
-    BF_DIE();
-    // tmp = bfCholCsrRealSolveVec(Mfact, v);
+    tmp = bfCholFacSolveVec(context.MFactCholesky, v, /* transposed: */ false);
   }
-  BfVec *tmp1 = chebmul(cheb, L, Mfact, tmp, lumping);
+  BfVec *tmp1 = chebmul(tmp);
   bfVecDelete(&tmp);
-  tmp = chebmul(cheb, L, Mfact, tmp1, lumping);
+  tmp = chebmul(tmp1);
   bfVecDelete(&tmp1);
   tmp1 = NULL;
-  if (lumping) {
-    tmp1 = bfMatMulVec(Mfact, tmp);
+  if (context.lumping) {
+    tmp1 = bfMatMulVec(context.MFactLumped, tmp);
   } else {
     BF_DIE();
-    // tmp1 = bfCholCsrRealSolveVec(Mfact, tmp); // TODO: needs to be C^{-T} not C^{-1} !
+    tmp1 = bfCholFacSolveVec(context.MFactCholesky, tmp, /* transposed: */ true);
   }
   bfVecDelete(&tmp);
   return tmp1;
 }
 
-static void get_factM(BfMat *M, BfMat **MfactHandle, int lumping) {
-  if (lumping) {
-    BfSize n = bfMatGetNumRows(M);
+int main(int argc, char const *argv[]) {
+  bfSeed(0); // must seed before using PRNG
 
-    BfMatCsrReal const *MCsr = bfMatConstToMatCsrRealConst(M);
+  if (argc < 5) {
+    printf("usage: %s mesh.obj kappa nu num_samples [p] [lumping] \n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  context.p = argc > 5 ? strtod(argv[5], NULL) : 128;
+  context.lumping = argc > 6 ? atoi(argv[6]) : 0;
+  context.kappa = atof(argv[2]);
+  context.nu = atof(argv[3]);
+  context.numSamples = atoi(argv[4]);
+  context.trimesh = bfTrimeshNewFromObjFile(argv[1]);
+  context.n = bfTrimeshGetNumVerts(context.trimesh);
+
+  printf("triangle mesh with %lu verts\n", context.n);
+
+  bfToc();
+
+  bfTrimeshGetLboFemDiscretization(context.trimesh, &context.L, &context.M);
+  printf("set up FEM discretization [%0.1fs]\n", bfToc());
+
+  context.lamMax = bfGetMaxEigenvalue(context.L, context.M);
+  printf("maximum eigenvalue: lambda = %g [%0.1fs]\n", context.lamMax, bfToc());
+
+  bfChebInitWithDegree(&context.gammaCheb, context.p, 0, context.lamMax);
+  bfChebInterp(&context.gammaCheb, gamma_, NULL);
+
+  /** Initialize MFact, taking into account whether we're doing mass lumping: */
+
+  if (context.lumping) {
+    BfMatCsrReal const *MCsr = bfMatConstToMatCsrRealConst(context.M);
 
     BfMatDiagReal *MLumpSqrtInv = bfMatDiagRealNew();
-    bfMatDiagRealInit(MLumpSqrtInv, n, n);
+    bfMatDiagRealInit(MLumpSqrtInv, context.n, context.n);
 
-    for (BfSize i = 0; i < n; ++i) {
+    for (BfSize i = 0; i < context.n; ++i) {
       MLumpSqrtInv->data[i] = 0;
       for (BfSize j = MCsr->rowptr[i]; j < MCsr->rowptr[i + 1]; ++j)
         MLumpSqrtInv->data[i] += MCsr->data[j];
       MLumpSqrtInv->data[i] = 1/sqrt(MLumpSqrtInv->data[i]);
     }
 
-    *MfactHandle = bfMatDiagRealToMat(MLumpSqrtInv);
+    context.MFactLumped = bfMatDiagRealToMat(MLumpSqrtInv);
   } else {
     BfCholCsrReal *MSqrt = bfCholCsrRealNew();
-    bfCholCsrRealInit(MSqrt, M);
-
-    BF_DIE();
-    // *MfactHandle = bfCholCsrRealToMat(MSqrt);
-  }
-}
-
-int main(int argc, char const *argv[]) {
-  if (argc < 5) {
-    printf("usage: %s mesh.obj kappa nu num_samples [p] [lumping] \n", argv[0]);
-    exit(EXIT_FAILURE);
+    bfCholCsrRealInit(MSqrt, context.M);
+    context.MFactCholesky = bfCholCsrRealToChol(MSqrt);
   }
 
-  BfSize p = argc > 5 ? strtod(argv[5], NULL) : 128;
-  int lumping = argc > 6 ? atoi(argv[6]) : 0;
-  kappa = atof(argv[2]);
-  nu = atof(argv[3]);
-  BfSize numSamples = atoi(argv[4]);
+  /** Evaluate gamma and our Chebyshev approximation of gamma on a
+   ** grid, and write the grid, the true gamma values, and the
+   ** polynomial values to disk for plotting. */
 
-  bfSeed(0); // must seed before using PRNG
-
-  BfTrimesh *trimesh = bfTrimeshNewFromObjFile(argv[1]);
-  BfSize numVerts = bfTrimeshGetNumVerts(trimesh);
-  printf("triangle mesh with %lu verts\n", numVerts);
-
-  bfToc();
-
-  BfMat *L = NULL, *M = NULL;
-  bfTrimeshGetLboFemDiscretization(trimesh, &L, &M);
-  printf("set up FEM discretization [%0.1fs]\n", bfToc());
-
-  BfReal lamMax = bfGetMaxEigenvalue(L, M);
-  printf("maximum eigenvalue: lambda = %g [%0.1fs]\n", lamMax, bfToc());
-
-  BfCheb gammaCheb;
-  bfChebInitWithDegree(&gammaCheb, p, 0, lamMax);
-  bfChebInterp(&gammaCheb, gamma_, NULL);
-
-  // /** Evaluate gamma and our Chebyshev approximation of gamma on a
-  //  ** grid, and write the grid, the true gamma values, and the
-  //  ** polynomial values to disk for plotting. */
-
-  // BfSize N = 1000;
-  // FILE *fp = NULL;
+  BfSize N = 1000;
+  FILE *fp = NULL;
 
   char filename[50];
-  // sprintf(filename, "lambda_p%i.bin", (int)p);
-  // fp = fopen(filename, "w");
-  // for (BfSize i = 0; i <= N; ++i) {
-  //   BfReal lam = (i*lamMax)/N;
-  //   fwrite(&lam, sizeof(BfReal), 1, fp);
-  // }
-  // fclose(fp);
+  sprintf(filename, "lambda_p%lu.bin", context.p);
 
-  // sprintf(filename, "gamma_p%i.bin", (int)p);
-  // fp = fopen(filename, "w");
-  // for (BfSize i = 0; i <= N; ++i) {
-  //   BfReal y = gamma_((i*lamMax)/N);
-  //   fwrite(&y, sizeof(BfReal), 1, fp);
-  // }
-  // fclose(fp);
+  fp = fopen(filename, "wb");
+  for (BfSize i = 0; i <= N; ++i) {
+    BfReal lam = (i*context.lamMax)/N;
+    fwrite(&lam, sizeof(BfReal), 1, fp);
+  }
+  fclose(fp);
 
-  // sprintf(filename, "gamma_cheb_p%i.bin", (int)p);
-  // fp = fopen(filename, "w");
-  // for (BfSize i = 0; i <= N; ++i) {
-  //   BfReal y = bfChebEval(&gammaCheb, (i*lamMax)/N);
-  //   fwrite(&y, sizeof(BfReal), 1, fp);
-  // }
-  // fclose(fp);
+  sprintf(filename, "gamma_p%lu.bin", context.p);
+  fp = fopen(filename, "wb");
+  for (BfSize i = 0; i <= N; ++i) {
+    BfReal y = gamma_((i*context.lamMax)/N);
+    fwrite(&y, sizeof(BfReal), 1, fp);
+  }
+  fclose(fp);
 
-  /** Get S = sqrt(M)^-T*L*sqrt(M)^-1 */
-
-  BfMat *Mfact = NULL;
-  get_factM(M, &Mfact, lumping);
+  sprintf(filename, "gamma_cheb_p%lu.bin", context.p);
+  fp = fopen(filename, "wb");
+  for (BfSize i = 0; i <= N; ++i) {
+    BfReal y = bfChebEval(&context.gammaCheb, (i*context.lamMax)/N);
+    fwrite(&y, sizeof(BfReal), 1, fp);
+  }
+  fclose(fp);
 
   /** Sample z once and write it out to disk for plotting. */
 
-  BfVec *z = sample_z(&gammaCheb, L, Mfact, lumping);
+  BfVec *z = sample_z();
 
-  sprintf(filename, "z_cheb_p%lu_kappa%.1e_nu%.1e.bin", p, kappa, nu);
+  sprintf(filename, "z_cheb_p%lu_kappa%.1e_nu%.1e.bin", context.p, context.kappa, context.nu);
   bfVecSave(z, filename);
   bfVecDelete(&z);
 
   /** Time how long it takes to sample z numSamples times. */
 
-  printf("computing %lu samples\n", numSamples);
+  printf("computing %lu samples\n", context.numSamples);
   bfToc();
-  for (BfSize _ = 0; _ < numSamples; ++_) {
-    z = sample_z(&gammaCheb, L, Mfact, lumping);
+  for (BfSize _ = 0; _ < context.numSamples; ++_) {
+    z = sample_z();
     bfVecDelete(&z);
   }
   double sampling_time = bfToc();
-  printf("drew %lu samples [%0.1fs]\n", numSamples, sampling_time);
+  printf("drew %lu samples [%0.1fs]\n", context.numSamples, sampling_time);
 
   // save factorization time and memory sizes to file
   char line[100];
   FILE *fptr;
-  sprintf(filename, "performance_kappa%.1e_nu%.1e.txt", kappa, nu);
-  sprintf(line, "%lu\t%.8e\n", p, sampling_time/numSamples);
+  sprintf(filename, "performance_kappa%.1e_nu%.1e.txt", context.kappa, context.nu);
+  sprintf(line, "%lu\t%.8e\n", context.p, sampling_time/context.numSamples);
   fptr = fopen(filename, "a");
   fprintf(fptr, line);
   fclose(fptr);
@@ -257,27 +256,26 @@ int main(int argc, char const *argv[]) {
   /** Evaluate the covariance function with respect to a fixed point
    ** on the mesh. */
 
-  BfVec *e = bfVecRealToVec(bfVecRealNewStdBasis(numVerts, 0));
-  BfVec *c = cov_matvec(e, &gammaCheb, L, Mfact, lumping);
-  sprintf(filename, "c_cheb_p%lu_kappa%.1e_nu%.1e.bin", p, kappa, nu);
+  BfVec *e = bfVecRealToVec(bfVecRealNewStdBasis(context.n, 0));
+  BfVec *c = cov_matvec(e);
+  sprintf(filename, "c_cheb_p%lu_kappa%.1e_nu%.1e.bin", context.p, context.kappa, context.nu);
   bfVecSave(c, filename);
 
   /* Compute and store covariance matrix vector products */
 
-  bfSeed(0);
-  BfSize s = numSamples;
-  BfMatDenseReal *randvecs = bfMatDenseRealNewZeros(numVerts, s);
-  BfMatDenseReal *matvecs = bfMatDenseRealNewZeros(numVerts, s);
 
-  printf("computing %lu matvecs with covariance\n", s);
-  for (BfSize j = 0; j < s; ++j) {
-      BfVec *x = bfVecRealToVec(bfVecRealNewRandn(numVerts));
+  BfMatDenseReal *randvecs = bfMatDenseRealNewZeros(context.n, context.numSamples);
+  BfMatDenseReal *matvecs = bfMatDenseRealNewZeros(context.n, context.numSamples);
+
+  printf("computing %lu matvecs with covariance\n", context.numSamples);
+  for (BfSize j = 0; j < context.numSamples; ++j) {
+      BfVec *x = bfVecRealToVec(bfVecRealNewRandn(context.n));
 
       // Set column of inputs:
       bfMatDenseRealSetCol(randvecs, j, x);
 
       // apply covariance matrix
-      BfVec *tmp1 = cov_matvec(x, &gammaCheb, L, Mfact, lumping);
+      BfVec *tmp1 = cov_matvec(x);
 
       // Set column of results:
       bfMatDenseRealSetCol(matvecs, j, tmp1);
@@ -287,20 +285,22 @@ int main(int argc, char const *argv[]) {
       bfVecDelete(&tmp1);
   }
 
-  sprintf(filename, "randvecs_cheb_p%lu_kappa%.1e_nu%.1e.bin", p, kappa, nu);
+  sprintf(filename, "randvecs_cheb_p%lu_kappa%.1e_nu%.1e.bin", context.p, context.kappa, context.nu);
   bfMatDenseRealSave(randvecs, filename);
 
-  sprintf(filename, "matvecs_cheb_p%lu_kappa%.1e_nu%.1e.bin", p, kappa, nu);
+  sprintf(filename, "matvecs_cheb_p%lu_kappa%.1e_nu%.1e.bin", context.p, context.kappa, context.nu);
   bfMatDenseRealSave(matvecs, filename);
 
   printf("cleaning up\n");
 
   /** Clean up: */
 
-  bfTrimeshDeinitAndDealloc(&trimesh);
-  bfMatDelete(&L);
-  bfMatDelete(&M);
-  bfChebDeinit(&gammaCheb);
-  bfVecDelete(&c);
-  bfMatDelete(&Mfact);
+  bfTrimeshDeinitAndDealloc(&context.trimesh);
+  bfMatDelete(&context.L);
+  bfMatDelete(&context.M);
+  bfChebDeinit(&context.gammaCheb);
+  if (context.lumping)
+    bfMatDelete(&context.MFactLumped);
+  else
+    bfCholDelete(&context.MFactCholesky);
 }
