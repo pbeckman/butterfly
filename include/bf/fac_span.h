11cf25975889a5b57b31a4e5b1b28b6c46b16c78
Samuel F. Potter <sfp@cims.nyu.edu>
2024-07-21 20:25:23 -0700

    add comment explaining FacSpan

--- include/bf/fac_span.h
+++ include/bf/fac_span.h
@@ -2,6 +2,17 @@
 
 #include "fac.h"
 
+/* A `FacSpan` is an array of column butterfly factorizations which
+ * are concatenated together in a row. This type exists in order to
+ * bundle together (possibly) partial butterfly factorizations as
+ * they're being streamed column by column.
+ *
+ * For example, if we have a binary column tree and we're
+ * incrementally constructing a butterfly factorization from left to
+ * right, we may not want to stream "a power-of-two"-many column
+ * blocks, in which case we can't push the butterfly "all the way
+ * through". This will leave a remainder of column blocks, and
+ * `FacSpan` can be used to group the results together. */
 struct BfFacSpan {
   BfSize numFacs;
   BfFac **fac;
